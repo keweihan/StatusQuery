4b7c48af8ae3994cbcb3b3e72ee39b8de77d2561
Kewei Han <55893673+keweihan@users.noreply.github.com>
2024-12-20 12:34:54 -0500

    Add non-deterministic server option

--- server/server.py
+++ server/server.py
@@ -14,7 +14,7 @@ TIME_RANGE = app.config.get("TIME_RANGE", 0)
 # Error configuration parameters
 ERROR_FREQ = app.config.get("ERROR_FREQUENCY", 0)
 ERROR_PROBABILITY = app.config.get("ERROR_PROBABILITY", 0)
-random.seed(app.config.get("RANDOM_SEED", 0))
+
 
 # Job state
 has_errored = False
@@ -22,6 +22,7 @@ start_time = -1
 
 
 def job_task():
+    """Simulate job that may error"""
     global has_errored
     global start_time
     start_time = time.time()
@@ -31,16 +32,16 @@ def job_task():
             has_errored = True
 
 
-# Start the job
 @app.route("/start", methods=["POST"])
 def start_job():
+    """Start the job"""
     threading.Thread(target=job_task, daemon=True).start()
     return jsonify({"result": "success"})
 
 
-# GET endpoint to return status
 @app.route("/status", methods=["GET"])
 def get_status():
+    """Return status of job"""
     if start_time == -1:
         return jsonify({"result": "error"})  # error if not started
     elif has_errored:
@@ -53,4 +54,6 @@ def get_status():
 
 
 if __name__ == "__main__":
+    if app.config.get("USE_SEED", True):
+        random.seed(app.config.get("RANDOM_SEED", 0))
     app.run(debug=app.config["DEBUG"], port=app.config["PORT"])
